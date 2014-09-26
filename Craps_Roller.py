d09723b517f1112f18e2e2bf404fc264a9d98cb6
troyscheet2 <tscheetner@stcharlessd.org>
2014-09-26 08:25:50 -0500

    edited file

--- Craps_Roller.py
+++ Craps_Roller.py
@@ -10,8 +10,8 @@ impart random
 die1 = random.randint(1, 6) 
 die2 = random.randrange(6)
 
-total = 'die1' + 'die2'
+total = die1 + die2
 
 print("You rolled a", die1, "and a", die2, "for a total of", total)
 
-input("\n\nPress the enter key to exit.")
+input("Press the enter key to exit.")
